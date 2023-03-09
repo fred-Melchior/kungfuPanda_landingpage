b908626366961b76838185ca6cad0779e327d38b
fred-Melchior <122490902+fred-Melchior@users.noreply.github.com>
2023-03-09 10:04:13 -0300

    header

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const heroSection = document.querySelector('.hero')
+    const alturaHero =  heroSection.clientHeight
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY
+
+        if (posicaoAtual > alturaHero){
+            ocultaHeader()
+        }
+        else{
+            exibeHeader()
+        }
+    })
+})
+
+function ocultaHeader(){
+    const header = document.querySelector('.header')
+    header.classList.add('header--is-hidden')
+}
+
+function exibeHeader(){
+    const header = document.querySelector('.header')
+    header.classList.remove('header--is-hidden')
+}

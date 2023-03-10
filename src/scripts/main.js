5554d5ace1733936db6cf9728b157ae5ce03aaf3
fred-Melchior <122490902+fred-Melchior@users.noreply.github.com>
2023-03-10 10:14:41 -0300

    faq footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', function(){
     const heroSection = document.querySelector('.hero')
     const alturaHero =  heroSection.clientHeight
 
+    const questions = document.querySelectorAll('[data-faq-question]')
+    
     window.addEventListener('scroll', function(){
         const posicaoAtual = window.scrollY
-
+        
         if (posicaoAtual > alturaHero){
             ocultaHeader()
         }
@@ -12,8 +14,13 @@ document.addEventListener('DOMContentLoaded', function(){
             exibeHeader()
         }
     })
+    
+    for (let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreFecha)
+    }
 })
 
+//Function Header:
 function ocultaHeader(){
     const header = document.querySelector('.header')
     header.classList.add('header--is-hidden')
@@ -22,4 +29,11 @@ function ocultaHeader(){
 function exibeHeader(){
     const header = document.querySelector('.header')
     header.classList.remove('header--is-hidden')
+}
+//Function Faq:
+function abreFecha(elemento){
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = elemento.target.parentNode
+    
+    elementoPai.classList.toggle(classe)
 }
